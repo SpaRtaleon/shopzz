87a408178b2982d397f09dcb2a97fb1d552abc8b
SpaRtaleon <raghubalan2972001@gmail.com>
2021-09-25 18:12:01 +0530

    set like main

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { RouterEvent, ROUTES } from '@angular/router';
 import { AccountService } from '';
 
 @Component({ templateUrl: 'home.component.html' })
-=======
-import { Route } from '@angular/compiler/src/core';
-import { FormGroup, FormControl, NgForm } from '@angular/forms';
-import { RouterEvent, ROUTES } from '@angular/router';
-import { Router } from '@angular/router';
-
->>>>>>> main
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-<<<<<<< HEAD
   title = 'App Name';
-=======
-  router: any;
-  loginrout(){
-    this.router.navigateByUrl('login')
-}  title = 'App Name';
->>>>>>> main
 
   showFiller = false;
 headers=[
@@ -34,9 +19,6 @@ headers=[
   {
       'firstname':'Fashion'
     }];
-<<<<<<< HEAD
-
-=======
     // name:string='';
     // passwd:any=''
     ngOnInit(){
@@ -46,19 +28,10 @@ headers=[
     
   
   
->>>>>>> main
     image1="../assets/img/shop.jpg"
     image2="../assets/img/wepik-2021822-17421.png"
     image3="../assets/img/pexels-terje-sollie-298863.jpg"
     images=[this.image1,this.image2,this.image3];
-<<<<<<< HEAD
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-=======
 
   constructor() { }
->>>>>>> main
 }
